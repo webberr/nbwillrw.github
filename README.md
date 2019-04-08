d9d3fee4ffd738c9667590c38a1f43810c333cec
webberr <webberr@gmail.com>
2019-04-07 19:53:35 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# nbwillrw.github
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/github/nbwillrw)
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,10 @@
+import { Component, AfterViewInit, ViewChild, TemplateRef, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent {
+}
